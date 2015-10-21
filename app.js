e872b64ce71afff3977b542226274bcb5d1eaeb5
adpatel312 <ankurpatel2015@u.northwestern.edu>
2015-10-21 18:14:01 -0500

    Update app.js

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ var numUsers = 0;
 io.on('connection', function (socket) {
   var addedUser = false;
 
-  // when the client emits 'new message', this listens and executes
+  
   socket.on('new message', function (data) {
     // we tell the client to execute 'new message'
     socket.broadcast.emit('new message', {
@@ -30,7 +30,7 @@ io.on('connection', function (socket) {
     });
   });
 
-  // when the client emits 'add user', this listens and executes
+  
   socket.on('add user', function (username) {
     // we store the username in the socket session for this client
     socket.username = username;
@@ -41,35 +41,35 @@ io.on('connection', function (socket) {
     socket.emit('login', {
       numUsers: numUsers
     });
-    // echo globally (all clients) that a person has connected
+    // connected
     socket.broadcast.emit('user joined', {
       username: socket.username,
       numUsers: numUsers
     });
   });
 
-  // when the client emits 'typing', we broadcast it to others
+  
   socket.on('typing', function () {
     socket.broadcast.emit('typing', {
       username: socket.username
     });
   });
 
-  // when the client emits 'stop typing', we broadcast it to others
+  // when the client emits 
   socket.on('stop typing', function () {
     socket.broadcast.emit('stop typing', {
       username: socket.username
     });
   });
 
-  // when the user disconnects.. perform this
+  // when the user disconnects
   socket.on('disconnect', function () {
     // remove the username from global usernames list
     if (addedUser) {
       delete usernames[socket.username];
       --numUsers;
 
-      // echo globally that this client has left
+      
       socket.broadcast.emit('user left', {
         username: socket.username,
         numUsers: numUsers
